4c9de0bc4b12aef10bacfa24a7f340b7c9dfcd50
apolishuk <apolishuk@Mac-Pro-User.local>
2019-03-12 16:48:51 +0300

    Добавлена основная навигация

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,9 +1,64 @@
-import { createAppContainer, createDrawerNavigator } from 'react-navigation';
-import MainTabNavigator from './MainTabNavigator';
+import React from 'react';
+import { createAppContainer, createDrawerNavigator, createBottomTabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const navTabs = {
-  Passengers: 'Пассажиры'
-}
-export default createAppContainer(createDrawerNavigator({
-  [navTabs.Passengers]: MainTabNavigator,
+import MainTabNavigator from './MainTabNavigator';
+import { isIOS } from '../utils/is';
+
+/** Навигация с табами для iOS и боковым меню для Android */
+const createMainNavigator = isIOS ? createBottomTabNavigator : createDrawerNavigator;
+
+export default createAppContainer(createMainNavigator({
+  Avia: {
+    screen: MainTabNavigator,
+    navigationOptions: {
+        title: 'Авиа',
+        [isIOS ? 'tabBarIcon' : 'drawerIcon']: ({ tintColor }) => (
+          <Icon 
+            name={`${isIOS ? 'ios' : 'md'}-airplane`} 
+            size={20}
+            color={tintColor}
+          />
+        ),
+    }
+  },
+  Trains: {
+    screen: MainTabNavigator,
+    navigationOptions: {
+        title: 'Поезда',
+        [isIOS ? 'tabBarIcon' : 'drawerIcon']: ({ tintColor }) => (
+          <Icon 
+            name={`${isIOS ? 'ios' : 'md'}-train`} 
+            size={20}
+            color={tintColor}
+          />
+        )
+    }
+  },
+  Hotels: {
+    screen: MainTabNavigator,
+    navigationOptions: {
+        title: 'Отели',
+        [isIOS ? 'tabBarIcon' : 'drawerIcon']: ({ tintColor }) => (
+          <Icon 
+            name={`${isIOS ? 'ios' : 'md'}-bed`} 
+            size={20}
+            color={tintColor}
+          />
+        )
+    }
+  },
+  Buses: {
+    screen: MainTabNavigator,
+    navigationOptions: {
+        title: 'Автобусы',
+        [isIOS ? 'tabBarIcon' : 'drawerIcon']: ({ tintColor }) => (
+          <Icon 
+            name={`${isIOS ? 'ios' : 'md'}-bus`} 
+            size={20}
+            color={tintColor}
+          />
+        )
+    }
+  },
 }));
